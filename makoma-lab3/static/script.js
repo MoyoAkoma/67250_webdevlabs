f44054ae83f513e58d5e6a270a3861913f9e7fff
MoyoAkoma <makoma@andrew.cmu.edu>
2025-03-12 15:11:52 -0400

    Steps 4 + 5 - xyAB and SumNPrint

--- makoma-lab3/static/script.js
+++ makoma-lab3/static/script.js
@@ -1,3 +1,11 @@
-<script>
-    
-</script>
+var x=5; var y=7; var z=x+y; 
+var A="Hello "; var B="world!"; var C= A+B; //concatinate?
+
+
+
+function SumNPrint(x1,x2){
+    var x3=x1+x2;
+    console.log(x3);
+}
+SumNPrint(x,y);
+SumNPrint(A,B);
